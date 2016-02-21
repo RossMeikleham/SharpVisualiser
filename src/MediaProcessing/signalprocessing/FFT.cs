c6029e8e96ef11a5403bcb183a4145534735e874
RossMeikleham <RossMeikleham@hotmail.co.uk>
2016-02-21 16:07:19 +0000

    Initial Commit

---
+++ src/MediaProcessing/signalprocessing/FFT.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Diagnostics;
+
+// Fast Fourier Transform Implementation
+namespace SharpPlayer.MediaProcessing.SignalProcessing {
+
+
+    public class FFT {
+
+        /* Performs a Bit Reversal Algorithm on a postive integer 
+         * for given number of bits
+         * e.g. 011 with 3 bits is reversed to 110 */ 
+        private static int BitReverse(int n, int bits) {
+            int reversedN = n;
+            int count = bits - 1;
+
+            n >>= 1;
+            while (n > 0) {
+                reversedN = (reversedN << 1) | (n & 1);
+                count--;
+                n >>= 1;
+            }
+
+            return ((reversedN << count) & ((1 << bits) - 1));
+        }
+
+
+        /* Uses Cooley–Tukey algorithm with radix-2 DIT case
+         * assumes no of points are a power of 2 */
+        public static Complex[] PerformFFT(Complex[] buffer) {
+            var result = new Complex[buffer.Length];
+            Array.Copy(buffer, result, buffer.Length);
+
+            /* After Processing, alternate odd/even pairs are out of order, e.g.
+             * after first iteration for 8 points: [x0,x2,x1,x3,x4,x6,x5,x7]
+             * The correct positions can be
+             * calculated by reversing the bits for the current position*/
+
+            int bits = (int)Math.Log(buffer.Length, 2);
+            for (int j = 1; j < buffer.Length / 2; j++) {
+
+                int swapPos = BitReverse(j, bits);
+                var temp = result[j];
+                result[j] = result[swapPos];
+                result[swapPos] = temp;
+            }
+
+            for (int N = 2; N <= buffer.Length; N <<= 1) {
+                
+                // Process all split splices of points; (TotalNoPoints / 2^n) splices at iteration n 
+                // e.g. for points [1,2,3,4,5,6,7,8]:
+                //
+                // n : 1 -> 4 splices [1,5],[3,7],[2,6],[4,8]
+                // n : 2 -> 2 splices [1,3,5,7],[2,4,6,8]
+                // n : 3 -> 1 splice [1,2,3,4,5,6,7,8]
+
+                for (int i = 0; i < buffer.Length; i+=N) {
+
+                    for (int k = 0; k < N / 2; k++) {
+
+                        int evenIndex = i + k;
+                        int oddIndex = i + k + (N / 2);
+                        var even = result[evenIndex];
+                        var odd = result[oddIndex];
+
+                        double term = -2 * Math.PI * k/ (double)N;
+                        Complex exp = new Complex(Math.Cos(term), Math.Sin(term)) * odd;
+
+                        result[evenIndex] = even + exp;
+                        result[oddIndex] = even - exp;
+                   
+                    }
+                }  
+            }
+
+            return result;
+        }
+
+
+
+
+     
+    }
+}
---
+++ tests/tests/MediaProcessing/SignalProcessing/FFTTest.cs
@@ -0,0 +1,117 @@
+
+// Tests For Fast Fourier Transform
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using SharpPlayer.MediaProcessing.SignalProcessing;
+
+
+namespace SharpPlayerTests {
+
+    [TestClass]
+    public class FFTTest {
+
+        /* Test FFT with no points */
+        [TestMethod]
+        public void FFTEmpty() {
+            var emptyArr = new Complex[0];
+            Assert.AreEqual(emptyArr.Length, FFT.PerformFFT(emptyArr).Length);
+        }
+
+
+        /* Test FFT with single point, should return the same point */
+        [TestMethod]
+        public void FFT1() {
+            
+            Random randNum = new Random();
+            Complex[] points = 
+                Enumerable.Repeat(0, 20)
+                          .Select(i => new Complex(randNum.NextDouble() * (10 ^ 6), 
+                                                    randNum.NextDouble() * (10 ^ 6)))
+                          .ToArray();
+
+            var singlePoint = new Complex[1];
+            foreach (Complex point in points) {
+                singlePoint[0] = point;
+                Assert.AreEqual(point, FFT.PerformFFT(singlePoint)[0]);
+            }
+        }
+
+
+        /* From Manual Calculation, FFT for N = 2 of points {x0, x1}
+         * should result in {x0 + x1, x0 - x1} */ 
+        [TestMethod]
+        public void FFT2() {
+            double[] realPoints = {1, 2.34, 78.94, 432987746, 3.14159, -32,      -99932, 1,     0,   0};
+            double[] imgPoints =  {0, 0,    0,     323,      -32,      43.32234, 3.163, 832, -0.003, 0};
+
+            var fValues = new Complex[realPoints.Length/2][];
+            var fResult = new Complex[realPoints.Length/2][];
+
+            for (int i = 0; i < fValues.GetLength(0); i++) {
+                fValues[i] = new Complex[2];
+                fValues[i][0] = new Complex(realPoints[2 * i], imgPoints[2 * i]);
+                fValues[i][1] = new Complex(realPoints[2 * i + 1], imgPoints[2 * i + 1]);
+
+                fResult[i] = FFT.PerformFFT(fValues[i]);
+            }
+
+            for (int i = 0; i < fValues.Length; i++) {
+                Assert.AreEqual(fValues[i][0] + fValues[i][1], fResult[i][0]);
+                Assert.AreEqual(fValues[i][0] - fValues[i][1], fResult[i][1]);
+            }
+        }
+
+
+        /* Test FFT with 4 Real points */
+        [TestMethod]
+        public void realFFT4() {
+            var values = new Complex[4];
+            for (int i = 1; i < values.Length + 1; i++) {
+                values[i - 1] = new Complex(i, 0);
+            }
+
+            double[] expectedReal ={ 10, -2, -2, -2, -2 };
+            double[] expectedImg = {0, 2, 0, -2};
+
+            var result = FFT.PerformFFT(values);
+            for (int i = 0; i < result.Length; i++) {
+                Assert.AreEqual(expectedReal[i], result[i].Real, delta: Math.Pow(10, -5));
+                Assert.AreEqual(expectedImg[i], result[i].Imaginary, delta: Math.Pow(10, -5));
+            }
+      
+        }
+
+
+        /* Test FFT with 8 Real points */
+        [TestMethod]
+        public void realFFT8() {
+            var values = new Complex[8];
+            for (int i = 0; i < values.Length; i++) {
+                values[i] = new Complex(i, 0);
+            }
+
+            double[] expectedReal =  
+                Enumerable.Repeat(28.0, 1)
+                          .Concat(Enumerable.Repeat(-4.0, values.Length - 1))
+                          .ToArray();
+
+            double[] expectedImg = { 0, 9.656854, 4, 1.656854, 0, -1.656854, -4, -9.656854 };
+
+            var result = FFT.PerformFFT(values);
+            for (int i = 0; i < result.Length; i++) {
+                Assert.AreEqual(expectedReal[i], result[i].Real, delta: Math.Pow(10, -5));
+                Assert.AreEqual(expectedImg[i], result[i].Imaginary, delta: Math.Pow(10, -5));
+            }
+
+        }
+
+
+    }
+}
